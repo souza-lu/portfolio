03631e2cc0b80f551b5dfb74f0287cea0547a8ea
souza-lu <79863171+souza-lu@users.noreply.github.com>
2023-08-28 22:37:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var texto = "LUCIMARA SOUZA";
+var atraso = 200;
+var textoElemento = document.querySelector(".titulo-principal");
+
+
+var contadorDeLetras = 0;
+function escreverTexto() {
+  if (contadorDeLetras < texto.length) {
+    textoElemento.textContent += texto.charAt(contadorDeLetras);
+    contadorDeLetras++;
+    setTimeout(escreverTexto, atraso);
+  }
+}
+
+escreverTexto();
